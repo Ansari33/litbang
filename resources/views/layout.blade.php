1624b4d699263e3e6bd981da96f5dcbad40b572b
Ansari33 <51727994+Ansari33@users.noreply.github.com>
2022-06-16 13:51:28 +0700

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -97,42 +97,42 @@
                         <!-- Top Cart
                         ============================================= -->
                         <div id="top-cart" class="header-misc-icon d-none d-sm-block">
-                            <a href="/litbang-admin" id="top-cart-trigger"><i class="icon-line-lock"></i></a>
-                            <div class="top-cart-content">
-                                <div class="top-cart-title">
-                                    <h4>Shopping Cart</h4>
-                                </div>
-                                <div class="top-cart-items">
-                                    <div class="top-cart-item">
-                                        <div class="top-cart-item-image">
-                                            <a href="#"><img src="{{asset('images/shop/small/1.jpg')}}" alt="Blue Round-Neck Tshirt" /></a>
-                                        </div>
-                                        <div class="top-cart-item-desc">
-                                            <div class="top-cart-item-desc-title">
-                                                <a href="#">Blue Round-Neck Tshirt with a Button</a>
-                                                <span class="top-cart-item-price d-block">$19.99</span>
-                                            </div>
-                                            <div class="top-cart-item-quantity">x 2</div>
-                                        </div>
-                                    </div>
-                                    <div class="top-cart-item">
-                                        <div class="top-cart-item-image">
-                                            <a href="#"><img src="{{asset('images/shop/small/6.jpg')}}" alt="Light Blue Denim Dress" /></a>
-                                        </div>
-                                        <div class="top-cart-item-desc">
-                                            <div class="top-cart-item-desc-title">
-                                                <a href="#">Light Blue Denim Dress</a>
-                                                <span class="top-cart-item-price d-block">$24.99</span>
-                                            </div>
-                                            <div class="top-cart-item-quantity">x 3</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="top-cart-action">
-                                    <span class="top-checkout-price">$114.95</span>
-                                    <a href="#" class="button button-3d button-small m-0">View Cart</a>
-                                </div>
-                            </div>
+                            <a href="/litbang-admin" ><i class="icon-line-lock"></i></a>
+{{--                            <div class="top-cart-content">--}}
+{{--                                <div class="top-cart-title">--}}
+{{--                                    <h4>Shopping Cart</h4>--}}
+{{--                                </div>--}}
+{{--                                <div class="top-cart-items">--}}
+{{--                                    <div class="top-cart-item">--}}
+{{--                                        <div class="top-cart-item-image">--}}
+{{--                                            <a href="#"><img src="{{asset('images/shop/small/1.jpg')}}" alt="Blue Round-Neck Tshirt" /></a>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="top-cart-item-desc">--}}
+{{--                                            <div class="top-cart-item-desc-title">--}}
+{{--                                                <a href="#">Blue Round-Neck Tshirt with a Button</a>--}}
+{{--                                                <span class="top-cart-item-price d-block">$19.99</span>--}}
+{{--                                            </div>--}}
+{{--                                            <div class="top-cart-item-quantity">x 2</div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="top-cart-item">--}}
+{{--                                        <div class="top-cart-item-image">--}}
+{{--                                            <a href="#"><img src="{{asset('images/shop/small/6.jpg')}}" alt="Light Blue Denim Dress" /></a>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="top-cart-item-desc">--}}
+{{--                                            <div class="top-cart-item-desc-title">--}}
+{{--                                                <a href="#">Light Blue Denim Dress</a>--}}
+{{--                                                <span class="top-cart-item-price d-block">$24.99</span>--}}
+{{--                                            </div>--}}
+{{--                                            <div class="top-cart-item-quantity">x 3</div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <div class="top-cart-action">--}}
+{{--                                    <span class="top-checkout-price">$114.95</span>--}}
+{{--                                    <a href="#" class="button button-3d button-small m-0">View Cart</a>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
                         </div><!-- #top-cart end -->
 
                     </div>
