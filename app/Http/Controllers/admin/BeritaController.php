402fa6ec30998758b85544976589d9496223eacb
Ansari33 <51727994+Ansari33@users.noreply.github.com>
2022-12-30 09:39:50 +0700

    Update BeritaController.php

--- app/Http/Controllers/admin/BeritaController.php
+++ app/Http/Controllers/admin/BeritaController.php
@@ -121,7 +121,7 @@ class BeritaController extends Controller
                 $image = str_replace('data:image/'.$extension.';base64,', '', $image);
                 $image = str_replace(' ', '+', $image);
                 $ss[] = $src;
-                \File::put(base_path().'/files-attachment/berita/' .$namaFile.'.jpg', base64_decode($image));
+                \File::put('../public_html/files-attachment/berita/' .$namaFile.'.jpg', base64_decode($image));
                 $url = '/files-attachment/berita/' .$namaFile.'.jpg';
 
             }else{
@@ -131,7 +131,7 @@ class BeritaController extends Controller
                 $extension = \Arr::last($arr_ext);
 
                 $namaFile += '.'.$extension;
-                file_put_contents((base_path().'/files-attachment/berita/').$namaFile, $gambar);
+                file_put_contents(('../public_html/files-attachment/berita/').$namaFile, $gambar);
                 $url = '/files-attachment/berita/' .$namaFile.'.'.$extension;
 
             }
